678f9f622f13aaeeb0a4f03727ff10f0f9ca79a3
Steven Berdak <stevenberdak@gmail.com>
2018-07-31 22:09:04 -0700

    Joke renamed to JokeData, added a status code

---
+++ JokeFountain/src/main/java/com/stevenberdak/jokefountain/Models/JokeData.java
@@ -0,0 +1,24 @@
+package com.stevenberdak.jokefountain.Models;
+
+public class JokeData {
+
+    public final String jokeBody, optFollowup;
+    public final int statusCode;
+    public static final int STATUS_EMPTY = 0;
+    public static final int STATUS_ERROR = 1;
+    public static final int STATUS_OK = 2;
+
+    public JokeData(String body, String optResponse, int statusCode) {
+        this.jokeBody = body;
+        this.optFollowup = optResponse;
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String toString() {
+        return "JokeData{" +
+                "jokeBody='" + jokeBody + '\'' +
+                ", optFollowup='" + optFollowup + '\'' +
+                '}';
+    }
+}
